4b9da3bda3e8807eb7c158823587cf82f0793d5a
Arindam Mondal <99799037+AriCode01@users.noreply.github.com>
2022-02-19 11:43:07 +0000

    19/02/22

---
+++ python_recap/lists2.py
@@ -0,0 +1,10 @@
+list1 = ['a','x','b','c','e','a','l']
+list2 = ['a','x','b','c','e','a','l']
+
+list1.sort()
+print('list1 after usnig .sort() method : ', list1)
+
+sorted(list2)
+print('list2 after usnig sorted(list2) function : ', list2)
+
+print('sorted() fucntion creates a new sorted list with given list : ',sorted(list2))
